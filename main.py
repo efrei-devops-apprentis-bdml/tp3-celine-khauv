b55a707c98ab09919750af52d1d127758a74cb11
xulaing <khauv@live.fr>
2022-06-17 13:21:32 +0200

    magie

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from marshmallow import Schema, fields
 
+
 class WeatherQuerySchema(Schema):
     key1 = fields.Str(required=True)
     key2 = fields.Str(required=True)
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,17 +19,18 @@ schema = WeatherQuerySchema()
 api_key = os.environ['API_KEY']
 
 
-
 class Weather(Resource):
     def get(self):
         errors = schema.validate(request.args)
         print(request.args)
-        url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s" % (request.args['lat'], request.args['lon'], api_key)
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s" % (
+            request.args['lat'], request.args['lon'], api_key)
         response = requests.get(url)
         data = json.loads(response.text)
         return data
 
+
 api.add_resource(Weather, '/')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True,port=8081)
+    app.run(host='0.0.0.0', debug=True, port=80)
